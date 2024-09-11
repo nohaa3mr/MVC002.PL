04af58fae3fd8ccd137172d69a5248ee50f6f78b
HP <HP@NOHA>
2024-09-11 10:52:11 +0300

    Add project files.

---
+++ MVC002.DAl/Configuation/DapartmentConf.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MVC002.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC002.DAL.Configuation
+{
+    internal class DapartmentConf : IEntityTypeConfiguration<Department>
+    {
+        public void Configure(EntityTypeBuilder<Department> builder)
+        {
+            builder.Property(d => d.Id).UseIdentityColumn(10, 10);
+            builder.Property(d => d.Code).HasColumnType("nvarchar").HasMaxLength(50);
+            builder.Property(d => d.Name).HasColumnType("nvarchar").HasMaxLength(50);
+
+        }
+    }
+}
---
+++ MVC002.DAl/Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC002.DAL.Configuation;
+using MVC002.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC002.DAL.Data
+{
+    internal class AppDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = . ; Database = MVCApp; Trusted_Connection = true");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly( Assembly.GetExecutingAssembly());
+        }
+        public DbSet<Department> departments  { get; set; }
+    }
+}
